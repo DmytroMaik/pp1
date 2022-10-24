34719d7d01d39c6f204969e8b1aea33da3f49ce4
DmytroMaik <114917896+DmytroMaik@users.noreply.github.com>
2022-10-24 12:58:05 +0200

    Add files via upload

---
+++ 04-Subroutines/functios cw8.py
@@ -0,0 +1,7 @@
+def number(x):
+ for i in range(1,x+1):
+    print(i,end=" ") 
+
+number(15)
+print(" ")
+number(25)
---
+++ 04-Subroutines/m.py
@@ -0,0 +1,6 @@
+import modulefinder
+
+x = m.read_number()
+y = m.read_number()
+
+print(f'sum of numbers {x} and {y} is {x + y}')
